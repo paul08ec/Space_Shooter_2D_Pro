f3a712ab40469a6f016167151ceb6a3d6a92da59
unknown <paul08ec@gmail.com>
2024-05-30 16:59:57 -0400

    Adding player input to the project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // public or private variabler
+    // data type ( int ,float ,bool ,string)
+    //variable name
+    // optional value assigne
+
+    [SerializeField]
+    private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         // player position = new position
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3 (0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left *5* Time.deltaTime);  
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
     }
 }
